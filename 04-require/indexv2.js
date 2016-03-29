4f1f162179fbf11ac0b16504d791323fa527e444
Manuel Torres Gil <mtorres@ual.es>
2016-03-29 20:20:06 +0000

    exporting as key-value pairs

---
+++ 04-require/indexv2.js
@@ -0,0 +1,4 @@
+mylib = require('./mylibv2.js');
+
+mylib.f1();
+mylib.f2();
---
+++ 04-require/mylibv2.js
@@ -0,0 +1,16 @@
+// mylibv2.js
+// Exportar las funciones como lista de pares clave-valor.
+// La clave es el nombre de la función y el valor es el 
+// cuerpo de la función
+
+
+module.exports = {
+	f1: function() {
+		console.log("Escrito desde función 1");
+	},
+
+	f2: function() {
+		console.log("Escrito desde función 2");
+	}
+}
+
